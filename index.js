9bda278f8a8384e5c748593eef218605ea60a9b9
Zhanna <zhanna01992@gmail.com>
2022-10-17 22:04:38 +0300

    feat: add carbon-dating, extender-repeater, vigenere-cipher, recursive-depth solutions

--- index.js
+++
@@ -1,5 +0,0 @@
-const { repeater } = require('./src/extended-repeater.js');
-
-
-   console.log(repeater(true, { repeatTimes: 3, separator: '??? ', addition: false, additionRepeatTimes: 2, additionSeparator: '!!!' }),11, 'truefalse!!!false??? truefalse!!!false??? truefalse!!!false');
-      console.log(repeater(null, { repeatTimes: 3, separator: '??? ', addition: null, additionRepeatTimes: 3, additionSeparator: '!!!' }),22, 'nullnull!!!null!!!null??? nullnull!!!null!!!null??? nullnull!!!null!!!null');
